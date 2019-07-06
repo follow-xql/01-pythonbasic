2fa5936a41702f8e9a8ed8876fb7b57cfe36cf27
follow-xql <52130793+follow-xql@users.noreply.github.com>
2019-07-06 17:11:24 +0800

    Create ex3.py

---
+++ ex3.py
@@ -0,0 +1,19 @@
+print("I will now count my chickens:")
+#打印一句话
+print("Hens",25+30/6)
+#后面可以直接计算，不需要引号，
+print("Roosters",100-25*3%4)
+print("Now I will count the eggs:")
+#直接计算结果，不需要引号
+print(3+2+1-5+4%2-1/4+6)
+print("IS it ture that 3+2<5-7?")
+#可以得到布尔值并且直接计较计算即可，不用引号，下同
+print(3+2<5-7)
+print("What is 3+2?",3+2)
+print("What is 5-7?",5-7)
+print("Oh,that's why it's False,")
+print("How about some more.")
+print("Is it greater?",5 > -2)
+print("Is it greater or equal?",5 >= -2)
+print("Is it less or equal?",5 <= -2)
+# 注，运算符前后尽量使用空格隔开
