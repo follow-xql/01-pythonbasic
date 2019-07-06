e97eee5d5fbb9ad6177ffb02fdbbdab10b10aa66
follow-xql <52130793+follow-xql@users.noreply.github.com>
2019-07-06 16:43:41 +0800

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,8 @@
+#A comment,this is so you can read your program later.
+#Anything after thr
+#is ignored by python.
+print ("I could have code like this,")#and the comment after is ignored
+#You can also use a comment to "disablea" or comment out code:
+#print("This won't run.")
+#print('nihao')
+print("This will run.")
