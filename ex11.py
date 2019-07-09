28efdc9d7dc0d9cd59757559f608a690bd5f080e
follow-xql <52130793+follow-xql@users.noreply.github.com>
2019-07-09 10:41:28 +0800

    Add files via upload

---
+++ ex11.py
@@ -0,0 +1,11 @@
+print("How old are you?",end='')
+age = input()
+print("How tall are you?",end='')
+height = input()
+print("How much do you weight?",end='')
+weight = input()
+
+print(f"SO,you're {age} old,{height} tall and {weight} heavy.")
+
+x=int(input())
+print(x)
